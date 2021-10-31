06b418dddd51af8d6b11e5664017b64085ef5b54
Matt Neuburg <matt@tidbits.com>
2021-10-31 10:50:10 -0700

    finished all the swiftification of AppController I intend to do, seems to be working

--- Doorman/AppController.swift
+++ Doorman/AppController.swift
@@ -34,8 +34,87 @@ import AppKit
     @IBOutlet weak var doormanMenu : NSMenu!
     @IBOutlet weak var updateMenuItem : NSMenuItem!
 
-    @IBAction func createPasswordClicked(_ sender: AnyObject) {}
-    @IBAction func optionCheckBoxClicked(_ sender: AnyObject) {}
+    @IBAction func createPasswordClicked(_ sender: AnyObject) {
+        self.copyButton.isEnabled = true
+
+        let specials = self.specialCharsTextField.stringValue
+        self.passwordHero.setSpecialCharsAsString(specials)
+        if specials.count == 0 {
+            self.specialCharsTextField.stringValue = self.passwordHero.specialCharsAsString()
+            self.hasSpecialCharsCheck.state = .off
+            self.optionCheckBoxClicked(self)
+        }
+
+        UserDefaults.standard.set(self.passwordHero.specialChars, forKey:"specialCharacters")
+
+        if self.passwordHero.isSpeakable {
+            self.passwordTextField.stringValue = self.passwordHero.createSpeakablePassword()
+        } else {
+            self.passwordTextField.stringValue = self.passwordHero.createPassword()
+        }
+
+        let strength = self.passwordHero.calculatePasswordStrength()
+        self.passwordStrengthLevelIndicator.integerValue = strength
+
+        if (strength < 4) {
+            self.strengthDescription.stringValue = NSLocalizedString("Unsecure", comment:"Unsecure")
+        } else if (strength < 6) {
+            self.strengthDescription.stringValue = NSLocalizedString("OK for web accounts", comment:"OK for web accounts")
+        }  else  if (strength < 9) {
+            self.strengthDescription.stringValue = NSLocalizedString("Safe for private data", comment:"Safe for private data")
+        }  else {
+            self.strengthDescription.stringValue = NSLocalizedString("Safe for confidential data", comment:"Safe for confidential data")
+        }
+
+//        self.favoriteButton.image = NSImage(named:"star_white")
+//        self.favoritesTableView.deselectAll(self)
+
+    }
+    @IBAction func optionCheckBoxClicked(_ sender: AnyObject) {
+        self.passwordHero.hasNumbers = self.hasNumbersCheck.state == .on
+        self.passwordHero.hasSpecialChars = self.hasSpecialCharsCheck.state == .on
+        self.passwordHero.hasUpperCase = self.hasUpperCaseCheck.state == .on
+        self.passwordHero.hasLowerCase = self.hasLowerCaseCheck.state == .on
+        self.passwordHero.isSpeakable = self.isSpeakableCheck.state == .on
+
+        if self.passwordHero.isSpeakable && !self.passwordHero.hasLowerCase && !self.passwordHero.hasUpperCase {
+            self.hasLowerCaseCheck.state = .on
+            self.passwordHero.hasLowerCase = true
+        }
+
+        var numberOfCheckedOptions = 0
+        if self.passwordHero.hasLowerCase {numberOfCheckedOptions += 1}
+        if self.passwordHero.hasUpperCase {numberOfCheckedOptions += 1}
+        if self.passwordHero.hasSpecialChars {numberOfCheckedOptions += 1}
+        if self.passwordHero.hasNumbers {numberOfCheckedOptions += 1}
+
+        if numberOfCheckedOptions < 2 {
+            self.hasLowerCaseCheck.isEnabled = self.passwordHero.hasLowerCase
+            self.hasUpperCaseCheck.isEnabled = self.passwordHero.hasUpperCase
+            self.hasSpecialCharsCheck.isEnabled = self.passwordHero.hasSpecialChars
+            self.hasNumbersCheck.isEnabled = self.passwordHero.hasNumbers
+        } else {
+            self.hasLowerCaseCheck.isEnabled = true
+            self.hasUpperCaseCheck.isEnabled = true
+            if self.passwordHero.isSpeakable {
+                self.hasLowerCaseCheck.isEnabled = self.passwordHero.hasUpperCase
+                self.hasUpperCaseCheck.isEnabled = self.passwordHero.hasLowerCase
+            }
+            self.hasNumbersCheck.isEnabled = true
+            self.hasSpecialCharsCheck.isEnabled = true
+        }
+
+        self.specialCharsTextField.isEnabled = self.passwordHero.hasSpecialChars
+
+        //in preferences speichern
+        let defaults = UserDefaults.standard
+        defaults.set(self.passwordHero.hasLowerCase, forKey:"hasLowerCase")
+        defaults.set(self.passwordHero.hasNumbers, forKey:"hasNumbers")
+        defaults.set(self.passwordHero.hasSpecialChars, forKey:"hasSpecialChars")
+        defaults.set(self.passwordHero.hasUpperCase, forKey:"hasUpperCase")
+        defaults.set(self.passwordHero.isSpeakable, forKey:"isSpeakable")
+
+    }
     @IBAction func checkForUpdate(_ sender: AnyObject) {}
     @IBAction func setLength(_ sender: AnyObject) {
         self.passwordHero.passwordLength = self.passwordLengthStepper.integerValue
@@ -43,7 +122,12 @@ import AppKit
         UserDefaults.standard.set(self.passwordHero.passwordLength, forKey:"passwordLength")
     }
     @IBAction func addToFavorites(_ sender: AnyObject) {}
-    @IBAction func copyPasswordToClipboard(_ sender: AnyObject) {}
+    @IBAction func copyPasswordToClipboard(_ sender: AnyObject) {
+        let password = passwordTextField.stringValue
+        let pb = NSPasteboard.general
+        pb.clearContents()
+        pb.setString(password, forType: .string)
+    }
 
     let passwordHero = PasswordHero()
 
@@ -85,10 +169,7 @@ import AppKit
         self.passwordLengthTextField.intValue = Int32(passwordHero.passwordLength)
 
         self.optionCheckBoxClicked(self)
-
-        createPasswordClicked(self)
-
-        
+        self.createPasswordClicked(self)
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
@@ -101,12 +182,5 @@ import AppKit
         }
         return .off
     }
-//    - (NSCellStateValue) stateFor:(bool) b {
-//        if (b) {
-//            return NSOnState;
-//        }
-//        return NSOffState;
-//    }
-
 
 }
